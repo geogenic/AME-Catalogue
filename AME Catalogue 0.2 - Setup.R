0a9491e0d7ab1b34d5c29a49f8a66756a12c91ad
Alessandro Mangiafico <40321580+geogenic@users.noreply.github.com>
2026-05-09 19:19:46 -0600

    Update AME Catalogue 2 - Setup.R
    Cleaned up the script for version 2 and added additional functionality. Be advised that my R scripts are largely obsolete now that better third party applications exist for Elite Dangerous.

--- AME Catalogue 0.2 - Setup.R
+++ AME Catalogue 0.2 - Setup.R
@@ -9,7 +9,7 @@
 # ROFL-13HM and ROFL-S134FIV
 # Type your prefix between the quotes 
 
-prefix <- ""
+prefix <- "AME"
 
 # The directory is where the AME Catalogue will look for your ED Discovery
 # spreadsheets, and where it'll export its own spreadsheets to. If you don't put
@@ -21,26 +21,22 @@ prefix <- ""
 directory <- ""
 
 # Below is where you put all your custom names for celestial objects.
-# I would highly recommend running the entire main program first (CTRL+SHIFT+S)
-# before you fill out this section so you can look up the new catalogue name
-# after they've been generated. After "names_cat <- c(" where it says "PUT YOUR NAMES HERE", 
-# put the name (from the CatRef column of the "AME Catalogue (Final).csv") of the object
-# you want to give a custom name to. Each entry should go on a separate line between quotes.
-# There should be a comma at the end of every line outside the quotes, except for after the last line.
+# After "names_cat <- c(" where it says "PUT YOUR NAMES HERE", 
+# put the body name (from the BodyName column of the "AME Catalogue (Final).csv") of the object
+# you want to give a custom name to. Each entry should go on a separate line between the quotes.
+# There should be a comma at the end of every line you put it, except for the last line.
 # Similarly, put your custom name entries on the lines between the parenthesis after
-# "names_custom <- c(". In each respective list, the name from the catalogue and the custom name must be
-# on the same line. For example, if the catalogue name is on the third line after "names_cat <- c(", the custom
-# name should be on the third line after "names_custom <- c(".
+# "names_custom <- c(".
 
 names_cat <- NA
 names_cat <- c(
-  PUT YOUR NAMES HERE
+  
   )
 names_cat <- as.data.frame(names_cat)
 
 names_custom <- NA
 names_custom <- c(
-  PUT YOUR CUSTOM NAMES HERE
+  
 )
 names_custom <- as.data.frame(names_custom)
 
@@ -48,11 +44,20 @@ names_custom <- as.data.frame(names_custom)
 
 # DON'T EDIT ANY OF THIS STUFF
 
+if (dir.exists("cache") == FALSE) {
+  dir.create("cache")
+}
+if (dir.exists("data") == FALSE) {
+  dir.create("data")
+}
+if (dir.exists("sound") == FALSE) {
+  dir.create("sound")
+}
 vars <- as.data.frame(NA)
 vars$prefix <- prefix
 vars$directory <- directory
-write.csv(vars, file = "vars.rvars")
-write.csv(names_cat, file = "names_cat.rvars")
-write.csv(names_custom, file = "names_custom.rvars")
+write.csv(vars, file = "cache/vars.rvars")
+write.csv(names_cat, file = "cache/names_cat.rvars")
+write.csv(names_custom, file = "cache/names_custom.rvars")
 
 ###############################################################################
